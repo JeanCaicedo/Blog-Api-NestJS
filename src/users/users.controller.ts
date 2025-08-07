ccca39b326fe89459ee5c44c440269f2d2d9d80e
JeanCaicedo <jeanz1912@hotmail.com>
2025-08-07 15:00:16 -0500

    Clase 01

---
+++ src/users/users.controller.ts
@@ -0,0 +1,38 @@
+﻿import { Controller, Get, Param } from '@nestjs/common';
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+@Controller('users')
+export class UsersController {
+  private users: User[] = [
+    {
+      id: '1',
+      name: 'Mariana Lopes  ',
+      email: 'john.doe@example.com',
+    },
+    {
+      id: '2',
+      name: 'Nicolas Lopes',
+      email: 'jane.doe@example.com',
+    },
+    {
+      id: '3',
+      name: 'Johann',
+      email: 'john.smith@example.com',
+    },
+  ];
+
+  @Get()
+  getUsers(): User[] {
+    return this.users;
+  }
+
+  @Get(':id')
+  findUser(@Param('id') id: string): User {
+    return this.users.find((user) => user.id === id) as User;
+  }
+}

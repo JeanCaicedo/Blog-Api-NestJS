9ac84f2b1c6f6a9a7419be7bedf6f0d986ca77de
JeanCaicedo <jeanz1912@hotmail.com>
2025-08-11 14:26:09 -0500

    Impro CRUD

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,7 @@
-﻿import { Controller, Get, Param } from '@nestjs/common';
+﻿import { Body, Controller, Get, NotFoundException, Param, Post, Delete } from '@nestjs/common';
 
 interface User {
-  id: string;
+  id: number;
   name: string;
   email: string;
 }
@@ -10,17 +10,17 @@ interface User {
 export class UsersController {
   private users: User[] = [
     {
-      id: '1',
+      id: 1,
       name: 'Mariana Lopes  ',
       email: 'john.doe@example.com',
     },
     {
-      id: '2',
+      id: 2,
       name: 'Nicolas Lopes',
       email: 'jane.doe@example.com',
     },
     {
-      id: '3',
+      id: 3,
       name: 'Johann',
       email: 'john.smith@example.com',
     },
@@ -33,6 +33,38 @@ export class UsersController {
 
   @Get(':id')
   findUser(@Param('id') id: string): User {
-    return this.users.find((user) => user.id === id) as User;
+    const userId = parseInt(id);
+    const user = this.users.find((user) => user.id === userId) as User;
+    if (!user) {
+      throw new NotFoundException('User not found');
+    } else {
+      return user;
+    }
+  }
+
+  @Post()
+  createUser(@Body() body: { name: string; email: string }): User {
+    // Generar automáticamente el siguiente ID disponible
+    const nextId = this.users.length > 0 ? Math.max(...this.users.map(user => user.id)) + 1 : 1;
+
+    const newUser: User = {
+      id: nextId,
+      name: body.name,
+      email: body.email,
+    };
+
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string): void {
+    const userId = parseInt(id);
+    const position = this.users.findIndex((user) => user.id === userId);
+    if (position === -1) {
+      throw new NotFoundException('User not found');
+    }
+    this.users.splice(position, 1);
+    return;
   }
 }
